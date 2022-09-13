353ab60046659b54c0de373cc6fb531243d718a5
“Daysha <+>
2022-09-13 13:00:53 -0700

    refactor donationsdetails to useSelector hook

--- src/components/Donation/DonationsDetails.js
+++ src/components/Donation/DonationsDetails.js
@@ -1,18 +1,21 @@
 import React, { useEffect } from 'react'
 import { Link} from 'react-router-dom'
-import { connect, useDispatch} from 'react-redux'
+import { connect, useSelector, useDispatch} from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
 import {getDonation, deleteDonation} from '../../redux/actions/donationsAction'
 import {BsFillBackspaceFill}  from "react-icons/bs";
 import { Button } from 'react-bootstrap'
 
-const DonationsDetails = ({getDonation, name, title, brand, size, department, image_url, available, shipping_price}) => {
+const DonationsDetails = ({getDonation}) => {
 
   const params = useParams()
   const donationId = params.id
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  const name = useSelector((state) => state.admin.name)
+  const donation = useSelector((state) => state.currentDonation)
 
   useEffect(() => {
     getDonation(donationId)
@@ -25,13 +28,13 @@ const DonationsDetails = ({getDonation, name, title, brand, size, department, im
 
   return (
       <div>  
-        <h4>{title}</h4>
-        <p>{brand}</p>
-        <p>{department}</p>
-        <p>{size}</p>
-        <img src={image_url} alt={title} />
-          <p>{available}</p>
-          <p>${shipping_price} </p>
+        <h4>{donation.title}</h4>
+        <p>{donation.brand}</p>
+        <p>{donation.department}</p>
+        <p>{donation.size}</p>
+        <img src={donation.image_url} alt={donation.title} />
+          <p>{donation.available}</p>
+          <p>${donation.shipping_price} </p>
           <Link to={`/donations`}>
           <Button variant="outline-secondary">View All<BsFillBackspaceFill/> </Button>
           </Link>
@@ -41,10 +44,5 @@ const DonationsDetails = ({getDonation, name, title, brand, size, department, im
       )
 }
 
-const mapStateToProps = (state) => {
-  return {...state.currentDonation,
-    name: state.admin.name}
-}
 
-
-export default connect(mapStateToProps, {getDonation})(DonationsDetails)
+export default connect(null, {getDonation})(DonationsDetails)
